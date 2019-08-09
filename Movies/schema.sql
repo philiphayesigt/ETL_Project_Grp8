97688d288534a6445a519f59cdff88b12420d26e
philiphayesigt <49686130+philiphayesigt@users.noreply.github.com>
2019-08-09 10:40:40 -0500

    Final movies commit

--- Movies/schema.sql
+++ Movies/schema.sql
@@ -1,11 +1,10 @@
 DROP TABLE movies;
 
 CREATE TABLE movies(
-	id SERIAL PRIMARY KEY,
+	movie_id VARCHAR(256),
 	title VARCHAR(256),
-	words_in_title VARCHAR(256),
 	imdb_rating FLOAT,
 	rating_count INT,
-	medium VARCHAR(256),
-	movie_index VARCHAR(256)
+	medium VARCHAR(256)
 );
+
